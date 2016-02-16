bed9399712e1265c8250168728c9c43e86cba7d0
Owais Zahid <owais.zahid@autodesk.com>
2016-02-16 16:47:31 +0800

    [PNG-0000] sample inventory app

---
+++ app/Product.ts
@@ -0,0 +1,22 @@
+/**
+ * A sample model class that represents a product in a inventory catalog system.
+ *
+ * Created by owais zahid <owais.zahid@autodesk.com> on 10/30/2015.
+ */
+export class Product {
+    entryDate: Date;
+    name: string;
+    description: string;
+    additionalComments: string;
+    serialNumber: number;
+    isExpired: boolean;
+
+    constructor(entryDate, name, description, additionalComments, serialNumber, isExpired) {
+        this.entryDate = entryDate;
+        this.name = name;
+        this.description = description;
+        this.additionalComments = additionalComments;
+        this.serialNumber = serialNumber;
+        this.isExpired = isExpired;
+    }
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,30 @@
 import {Component} from 'angular2/core';
+import {Product} from './Product';
+import {NgFor} from 'angular2/common';
+
 @Component({
     selector: 'my-app',
-    template: `<h1>My First Angular 2 App</h1>
-        <div role="listbox">
-            <paper-item>Inbox</paper-item>
-            <paper-item>Starred</paper-item>
-            <paper-item>Sent mail</paper-item>
-        </div>
-        <paper-input label="text input"></paper-input>
-    `
+    templateUrl: 'app/app.component.html'
 })
-export class AppComponent { }
+
+export class AppComponent {
+  products: Array<Product>;
+	selectedItem: Product;
+
+  constructor() {
+    this.products = [
+				new Product(new Date('10 Nov, 2013'), 'Brake', 'Car Hydrolics Brake', 'good safely record', '1000', false),
+				new Product(new Date('10 Jun, 2014'), 'Chassis', 'Mclaren Chassis', 'strong support', '2010', false)
+		];
+
+		this.selectedItem = this.products[0];
+  }
+
+  productClicked(product) {
+			this.selectedItem = product;
+	}
+
+	newProduct() {
+			this.products.push(this.selectedItem = new Product(new Date('10 Jun, 2014'), 'Feul Injector', 'Delphi FI', 'Multi-Port Fuel', '2010', false))
+	}
+}
